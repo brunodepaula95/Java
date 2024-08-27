308d2d55dec1f19c9e572bf620d021466c5593bc
Bruno de Paula <brunohpaula1995@gmail.com>
2024-08-26 21:43:31 -0300

    Metodos em Java

---
+++ Metodos/src/Usuario.java
@@ -0,0 +1,12 @@
+public class Usuario {
+
+    public static void main(String[] args) throws Exception{
+
+        Controle controle = new Controle();
+
+        System.out.println(controle.ligada);
+        System.out.println(controle.canal);
+        System.out.println(controle.volume);
+
+    }
+}
