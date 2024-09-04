f3d1ef46323101d3064bcd80bbea958442bbfb9d
Bruno de Paula <brunohpaula1995@gmail.com>
2024-09-03 21:50:58 -0300

    att

--- exemplo-pacotes/src/Enum/SistemaIbge.java
+++ exemplo-pacotes/src/Enum/SistemaIbge.java
@@ -2,7 +2,7 @@ package Enum;
 
 public class SistemaIbge {
     public static void main(String[] args) {
-      for(EstadoBrasileiro e: EstadoBrasileiro.values()){//o E é o nome da variavel e o : é como se fosse o simbolo de =
+      for(EstadoBrasileiro e: EstadoBrasileiro.values()){//o E é o nome da variavel e o : é como se fosse o simbolo de =, values é o metodo que retorna o array de elementos
         System.out.println(e.getSigla() + " - " + e.getNome());
       }
     }
