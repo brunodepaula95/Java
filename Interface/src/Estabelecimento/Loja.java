c66adf5e5fddd7ae5bb5c411ec64a2d02b2abb2c
Bruno de Paula <brunohpaula1995@gmail.com>
2024-09-04 00:34:10 -0300

    iNTERFACE

---
+++ Interface/src/Estabelecimento/Loja.java
@@ -0,0 +1,9 @@
+package Estabelecimento;
+
+public class Loja {
+
+  public static void main(String[] args) {
+    
+  }
+
+}
---
+++ Interface/src/equipamentos/multifuncional/EquipamentoMultifuncional.java
@@ -0,0 +1,20 @@
+package equipamentos.multifuncional;
+
+import equipamentos.copiadora.Copiadora;
+import equipamentos.digitalizadora.Digitalizadora;
+import equipamentos.impressora.Impressora;
+
+public class EquipamentoMultifuncional implements Copiadora, Digitalizadora, Impressora {//Quando precisa que uma classe tenha mais de um papel, o certo é implementar interfaces.
+  
+    public void copiar(){
+
+    }
+
+    public void digitalizar(){
+      
+    }
+
+    public void imprimir(){
+      
+    }
+}
